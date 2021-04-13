0cae22855561aba99b542017d71782c48b0dea50
Geanina Sandu <sandugeanina83@gmail.com>
2021-04-13 20:57:45 +0200

    Setup Mongo Atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+/* Open connection to Mongo Atlas */
+const mongoUri = process.env.MONGO_URI;
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+    .then(() => console.log('MongoDB database connection established successfully!'))
+    .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
